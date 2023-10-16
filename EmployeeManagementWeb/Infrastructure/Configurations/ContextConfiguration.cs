dd11d130b70ee2be9186909ec7819e84fc539e34
KirillValetko <kirya_valet@mail.ru>
2023-10-16 16:28:06 +0300

    added environment behaviour to context

--- EmployeeManagementWeb/Infrastructure/Configurations/ContextConfiguration.cs
+++ EmployeeManagementWeb/Infrastructure/Configurations/ContextConfiguration.cs
@@ -7,10 +7,20 @@ namespace EmployeeManagement.Web.Infrastructure.Configurations
     {
         private const string ConnectionString = "ConnectionStrings:EmployeeDB";
 
-        public static void InitDbContext(this IServiceCollection services, IConfiguration configuration)
+        public static void InitDbContext(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
         {
-            services.AddDbContext<EmployeeManagementContext>(opt =>
-                opt.UseSqlServer(configuration[ConnectionString]));
+            if (environment.IsDevelopment())
+            {
+                var str = configuration.GetSection(environment.EnvironmentName)[ConnectionString];
+                services.AddDbContext<EmployeeManagementContext>(opt =>
+                    opt.UseSqlServer(configuration[ConnectionString]));
+            }
+            else
+            {
+                var str = configuration.GetSection(environment.EnvironmentName)[ConnectionString];
+                services.AddDbContext<EmployeeManagementContext>(opt =>
+                    opt.UseNpgsql(configuration[ConnectionString]));
+            }
         }
     }
 }
